bc239e67edc42b75379997671435c18858cff3e3
Ryan W <ryan_645@outlook.com>
2020-05-08 19:50:01 -0400

    This is the first commit for new branch

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//this is for testing the psuh to new branch
+
 export default function App() {
   return (
     <View style={styles.container}>
